3cefaed1e07a167f6b539476ef9d5a545217a860
Eric Zila <zila.eric@gmail.com>
2026-02-14 12:35:39 +0100

    Add logic to create experiment results directory during initialization

--- src/deepfabm/utils/setup.py
+++ src/deepfabm/utils/setup.py
@@ -1,7 +1,28 @@
+import os
 from argparse import Namespace
+from datetime import datetime
 
 from .logger import LOGGER, setup_logging, setup_wandb, terminate_wandb
 from .seed import set_seed
+
+
+def _prepare_results_dir(setup: Namespace) -> None:
+    """
+    Create directory to store experiment results in. Reusable paths are saved to the
+    experimental setup namespace passed as `setup`.
+
+    :param setup: Experimental setup
+    :type setup: Namespace
+    """
+    # Create unique identifier for the current experiment
+    id = f"experiment_{datetime.now().strftime('%Y-%m-%mT%H:%M:%S')}"
+
+    # Create experiment results directory
+    resultsdir = os.path.join("results", id)
+    os.mkdir(resultsdir)
+
+    # Save path to results directory to experimental setup
+    setup.resultsdir = resultsdir
 
 
 def initialize_run(setup: Namespace) -> None:
@@ -14,6 +35,10 @@ def initialize_run(setup: Namespace) -> None:
     :param setup: Experimental setup
     :type setup: Namespace
     """
+    # Set up experimental results folder
+    if setup.command == "train":
+        _prepare_results_dir(setup)
+
     # Set up logging
     setup_logging(setup.loglevel)
     LOGGER.info(f"Dictionary of parsed arguments: {vars(setup)!r}")
