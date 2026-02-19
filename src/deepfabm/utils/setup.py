53dece59de0a3a5d2b742c1b135adac18795d2b0
Eric Zila <zila.eric@gmail.com>
2026-02-19 11:30:40 +0100

    Save final experimental setup to results folder (#44)

--- src/deepfabm/utils/setup.py
+++ src/deepfabm/utils/setup.py
@@ -2,13 +2,15 @@ import os
 from argparse import Namespace
 from datetime import datetime
 
+import yaml
+
 from .logger import LOGGER, setup_logging, setup_wandb, terminate_wandb
 from .seed import set_seed
 
 
 def _prepare_results_dir(setup: Namespace) -> None:
     """
-    Create directory to store experiment results in. Reusable paths are saved to the
+    Creates directory to store experiment results in. Reusable paths are saved to the
     experimental setup namespace passed as `setup`.
 
     :param setup: Experimental setup
@@ -33,19 +35,34 @@ def _prepare_results_dir(setup: Namespace) -> None:
     setup.logpath = logpath
 
 
+def _save_configuration(setup: Namespace) -> None:
+    """
+    Saves experimental setup passed as `setup` to the designated experiment results
+    folder in the `config.yaml` file.
+
+    :param setup: Experimental setup
+    :type setup: Namespace
+    """
+    configpath = os.path.join(setup.resultsdir, "config.yaml")
+    with open(configpath, "a") as f:
+        yaml.dump(vars(setup), f)
+
+
 def initialize_run(setup: Namespace) -> None:
     """
     Initializes run by setting up results directory and initializing all services.
     Adjusts namespace in place to contain information on the initialized assets.
 
-    :param script: Invoked script, either `"train"` or `"evaluate"`
-    :type script: str
     :param setup: Experimental setup
     :type setup: Namespace
     """
-    # Set up experimental results folder
+    # Set up experiment results folder
     if setup.command == "train":
         _prepare_results_dir(setup)
+
+    # Finalize configuration
+    if setup.command == "train":
+        _save_configuration(setup)
 
     # Set up logging
     setup_logging(setup.loglevel, getattr(setup, "logpath", None))
